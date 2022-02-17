aac0b6dd7281a84647fb9e494241b43db97c64e5
benjamin lloyd <benjamin.a.lloyd25@gmail.com>
2022-02-17 12:18:03 -0500

    changed settins and added local settings

--- heroes_villians_project/heroes_villians_project/settings.py
+++ heroes_villians_project/heroes_villians_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0=i_(q+8k#01u%1u-vd555se@(1traj6c%hwqm4xhnj(scbj37'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroes_villians_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +111,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from heroes_villians_project.local_settings import *
+except ImportError:
+    pass
